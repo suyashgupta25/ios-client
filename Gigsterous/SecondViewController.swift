beccc5d78de9036d649735521dd7eef442ff545f
Svacha, Michal <svachmic@fel.cvut.cz>
2016-06-29 17:58:40 +0200

    Initial Project Setup

---
+++ Gigsterous/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Gigsterous
+//
+//  Created by Svacha, Michal on 29/06/16.
+//  Copyright © 2016 Svacha, Michal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
