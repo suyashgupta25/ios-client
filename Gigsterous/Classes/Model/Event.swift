963fc563ff88809e5259476f89a23cc0a993e9e6
Svacha, Michal <svachmic@fel.cvut.cz>
2016-09-22 17:14:45 +0200

    Model classes stub

---
+++ Gigsterous/Classes/Model/Event.swift
@@ -0,0 +1,14 @@
+//
+//  Event.swift
+//  Gigsterous
+//
+//  Created by Svacha, Michal on 21/09/16.
+//  Copyright © 2016 Svacha, Michal. All rights reserved.
+//
+
+import Foundation
+
+/// Model class representing an event.
+class Event {
+    //
+}
---
+++ Gigsterous/Classes/Model/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  Gigsterous
+//
+//  Created by Svacha, Michal on 21/09/16.
+//  Copyright © 2016 Svacha, Michal. All rights reserved.
+//
+
+import Foundation
+
+/// Model class representing a person.
+class Person {
+    //
+}
