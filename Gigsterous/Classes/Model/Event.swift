e8f9d36f93c0d6f4947defee13d4156829e1281b
Michal Svacha <michal.svacha@gmail.com>
2016-10-25 15:18:28 +0200

    Form model validation

--- Gigsterous/Classes/Model/Event.swift
+++ Gigsterous/Classes/Model/Event.swift
@@ -19,11 +19,32 @@ class Event: Mappable {
     
     required init?(map: Map) {}
     
+    init() {}
+    
     func mapping(map: Map) {
         id <- map["id"]
         name <- map["name"]
         venue <- map["venue"]
         date <- (map["date"], DateStringTransform())
         people <- map["people"]
+    }
+    
+    /// Indicates whether or not the object has all necessary data set in order to be sent to the server.
+    ///
+    /// - Returns: True if all necessary data has been set (name, venue and date).
+    func isComplete() -> Bool {
+        guard self.name != nil else {
+            return false
+        }
+        
+        guard self.venue != nil else {
+            return false
+        }
+        
+        guard self.date != nil else {
+            return false
+        }
+        
+        return true
     }
 }
