8a139b748ec0756d2b6a7c51818aba2b4d0f22e5
Michal Svacha <michal.svacha@gmail.com>
2016-10-28 21:00:30 +0200

    Implemented MVVM

--- Gigsterous/Classes/Controllers/EventFormViewController.swift
+++ Gigsterous/Classes/Controllers/EventFormViewController.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import Bond
 
 ///
-class EventFormViewController: UITableViewController, DateTableViewCellDelegate, InputTableViewCellDelegate {
-    let event = Event()
+class EventFormViewController: UITableViewController {
+    let eventViewModel = EventFormViewModel()
     
     /// Instance variable keeping track of whether or not a date cell is expanded.
     ///
@@ -46,13 +47,9 @@ class EventFormViewController: UITableViewController, DateTableViewCellDelegate,
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.event.name = NSLocalizedString("NEW_EVENT", comment: "")
-        self.event.startDate = Date()
-        self.event.endDate = self.event.startDate.addingTimeInterval(3600)
-
-        self.navigationItem.title = NSLocalizedString("NEW_EVENT", comment: "")
-        self.tableView.tableFooterView = UIView(frame: CGRect.zero)
+        self.eventViewModel.name.bind(to: self.navigationItem.bnd_title)
         
+        self.tableView.tableFooterView = UIView(frame: CGRect.zero)
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(EventFormViewController.cancelButtonPressed))
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(EventFormViewController.doneButtonPressed))
     }
@@ -66,7 +63,7 @@ class EventFormViewController: UITableViewController, DateTableViewCellDelegate,
     }
     
     func doneButtonPressed() {
-        if self.event.isComplete() {
+        if self.eventViewModel.isComplete() {
             print("I AM COMPLETE!")
         } else {
             let alertController = UIAlertController(
@@ -121,28 +118,40 @@ class EventFormViewController: UITableViewController, DateTableViewCellDelegate,
         
         switch identifier {
             case "dateInput":
-                let dateCell = cell as! DateTableViewCell
-                dateCell.row = indexPath.row
-                dateCell.delegate = self
-                
-                let date = indexPath.row == 0 ?
-                    self.event.startDate : self.event.endDate
-                dateCell.setDate(date: date!)
-            break
-            case "textInput":
-                let inputCell = cell as! InputTableViewCell
-                inputCell.row = indexPath.row
-                inputCell.delegate = self
+                let label = cell.viewWithTag(2) as! UILabel
+                let datePicker = cell.viewWithTag(3) as! UIDatePicker
+                datePicker.minimumDate = Date()
                 
                 if indexPath.row == 0 {
-                    let textField = cell.viewWithTag(2) as! UITextField
-                    textField.text = self.event.name
+                    datePicker.setDate(self.eventViewModel.startDate.value, animated: false)
+                    datePicker.bnd_date.bidirectionalBind(to: self.eventViewModel.startDate)
+                    self.eventViewModel.startDate
+                        .map{$0.humanReadableString()}
+                        .bind(to: label.bnd_text)
+                } else {
+                    datePicker.setDate(self.eventViewModel.endDate.value, animated: false)
+                    datePicker.bnd_date.bidirectionalBind(to: self.eventViewModel.endDate)
+                    self.eventViewModel.endDate
+                        .map{$0.humanReadableString()}
+                        .bind(to: label.bnd_text)
+                }
+            break
+            case "textInput":
+                let textField = cell.viewWithTag(2) as! UITextField
+                
+                if indexPath.row == 0 {
+                    textField.text = self.eventViewModel.name.value
+                    textField.bnd_text.bind(to: self.eventViewModel.name)
+                } else {
+                    textField.text = self.eventViewModel.venue.value
+                    textField.bnd_text.bind(to: self.eventViewModel.venue)
                 }
             break
         default:
             break
         }
         
+        /// Localization of labels
         if let label = cell.viewWithTag(1) as? UILabel, let labelText = cellInfo["name"] {
             label.text = NSLocalizedString(labelText, comment: "")
         }
@@ -153,7 +162,13 @@ class EventFormViewController: UITableViewController, DateTableViewCellDelegate,
     // MARK: - Table view delegate methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let _ = tableView.cellForRow(at: indexPath) as? DateTableViewCell {
+        guard let cells = self.structure[indexPath.section]["cells"] as? [[String : String]] else {
+            fatalError("Cell structure not found")
+        }
+        let cellInfo = cells[indexPath.row]
+        let identifier = cellInfo["type"]!
+        
+        if identifier == "dateInput" {
             if self.isDateExpanded && !(indexPath.isEqual(other: self.expandedIndexPath)) {
                 self.isDateExpanded = !self.isDateExpanded
                 self.tableView.beginUpdates()
@@ -162,48 +177,10 @@ class EventFormViewController: UITableViewController, DateTableViewCellDelegate,
             
             self.isDateExpanded = !self.isDateExpanded
             self.expandedIndexPath = indexPath
-            
             self.tableView.beginUpdates()
             self.tableView.endUpdates()
         }
         
         tableView.cellForRow(at: indexPath)?.setSelected(false, animated: true)
-    }
-    
-    // MARK: - DateTableViewCell delegate method
-    
-    func onDidChangeDate(date: Date, row: Int) {
-        let formerDifference = self.event.endDate.timeIntervalSince(self.event.startDate)
-        
-        if row == 0 {
-            self.event.startDate = date
-            
-            if self.event.startDate.compare(self.event.endDate) != .orderedAscending {
-                self.event.endDate = self.event.startDate.addingTimeInterval(formerDifference)
-            }
-        } else {
-            self.event.endDate = date
-            
-            if self.event.startDate.compare(self.event.endDate) != .orderedAscending {
-                var dateCandidate = self.event.endDate.addingTimeInterval(-formerDifference)
-                if dateCandidate.compare(Date()) == .orderedAscending {
-                    dateCandidate = Date()
-                }
-                
-                self.event.startDate = dateCandidate
-            }
-        }
-        
-        self.tableView.reloadData()
-    }
-    
-    // MARK: - InputTableViewCell delegate method
-    
-    func onFinishedTyping(text: String, row: Int) {
-        if row == 0 {
-            self.event.name = text
-        } else {
-            self.event.venue = text
-        }
     }
 }
