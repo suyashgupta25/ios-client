54e60c6678ba13a7e8432809c4fb42268da5bb83
Michal Svacha <michal.svacha@gmail.com>
2016-10-30 18:20:38 +0100

    Code cleanup (DRY) + comments

--- Gigsterous/Classes/Controllers/EventFormViewController.swift
+++ Gigsterous/Classes/Controllers/EventFormViewController.swift
@@ -9,19 +9,20 @@
 import UIKit
 import Bond
 
-///
+/// Controller responsible for handling form for a new event.
 class EventFormViewController: UITableViewController {
     let eventViewModel = EventFormViewModel()
     
-    /// Instance variable keeping track of whether or not a date cell is expanded.
+    /// Instance variable keeping track of whether or not a date cell is expanded. If the value is nil, nothing is expanded.
     ///
-    /// - didSet: Subscribed to the value change and changes the enabled state of the Done button in order to prevent the user from submitting incomplete form.
-    var isDateExpanded = false {
+    /// - didSet: Subscribed to the value change and changes the enabled state of the Done button in order to prevent the user from submitting an incomplete form.
+    var expandedIndexPath: IndexPath? {
         didSet {
-            self.navigationItem.rightBarButtonItem?.isEnabled = !isDateExpanded
+            self.navigationItem.rightBarButtonItem?.isEnabled = expandedIndexPath == nil
         }
     }
-    var expandedIndexPath = IndexPath(row: 0, section: 0)
+    
+    // MARK: - Table view content structure
     
     let structure = [
         ["sectionName": "GENERAL",
@@ -43,6 +44,19 @@ class EventFormViewController: UITableViewController {
             ["name": "ADD_AUDITION", "type": "addInput"]]
         ],
     ]
+    
+    /// Parses and returns the structure of a cell at given indexPath
+    ///
+    /// - Returns: Dictionary containing information about the cell.
+    func cellInfo(for indexPath: IndexPath) -> [String : String] {
+        guard let cells = self.structure[indexPath.section]["cells"] as? [[String : String]] else {
+            fatalError("Cell structure not found")
+        }
+        
+        return cells[indexPath.row]
+    }
+    
+    // MARK: -
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,10 +72,14 @@ class EventFormViewController: UITableViewController {
         super.didReceiveMemoryWarning()
     }
     
+    // MARK: - IBActions
+    
+    /// Dismisses the controller and returns user to the previous screen. Called when cancel button in the top left corner is pressed.
     func cancelButtonPressed() {
         self.navigationController?.dismiss(animated: true, completion: nil)
     }
     
+    /// Checks whether information in the form is complete and proceeds with next steps. When information is not complete, warns the user. Called when done button in the top right corner is pressed.
     func doneButtonPressed() {
         if self.eventViewModel.isComplete() {
             print("I AM COMPLETE!")
@@ -93,7 +111,7 @@ class EventFormViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if self.isDateExpanded && indexPath.isEqual(other: self.expandedIndexPath) {
+        if let expanded = self.expandedIndexPath, expanded.isEqual(other: indexPath) {
             return 255
         }
         
@@ -109,10 +127,7 @@ class EventFormViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cells = self.structure[indexPath.section]["cells"] as? [[String : String]] else {
-            fatalError("Cell structure not found")
-        }
-        let cellInfo = cells[indexPath.row]
+        let cellInfo = self.cellInfo(for: indexPath)
         let identifier = cellInfo["type"]!
         let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
         
@@ -161,24 +176,24 @@ class EventFormViewController: UITableViewController {
     
     // MARK: - Table view delegate methods
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let cells = self.structure[indexPath.section]["cells"] as? [[String : String]] else {
-            fatalError("Cell structure not found")
+    /// Handles internal storage of currently expanded cell's indexPath. Called after an expandable (= date) cell has been pressed.
+    func handleDateExpansion(indexPath: IndexPath) {
+        if let expanded = self.expandedIndexPath, expanded.isEqual(other: indexPath) {
+            self.expandedIndexPath = nil
+        } else {
+            self.expandedIndexPath = indexPath
         }
-        let cellInfo = cells[indexPath.row]
+        
+        self.tableView.beginUpdates()
+        self.tableView.endUpdates()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cellInfo = self.cellInfo(for: indexPath)
         let identifier = cellInfo["type"]!
         
         if identifier == "dateInput" {
-            if self.isDateExpanded && !(indexPath.isEqual(other: self.expandedIndexPath)) {
-                self.isDateExpanded = !self.isDateExpanded
-                self.tableView.beginUpdates()
-                self.tableView.endUpdates()
-            }
-            
-            self.isDateExpanded = !self.isDateExpanded
-            self.expandedIndexPath = indexPath
-            self.tableView.beginUpdates()
-            self.tableView.endUpdates()
+            self.handleDateExpansion(indexPath: indexPath)
         }
         
         tableView.cellForRow(at: indexPath)?.setSelected(false, animated: true)
