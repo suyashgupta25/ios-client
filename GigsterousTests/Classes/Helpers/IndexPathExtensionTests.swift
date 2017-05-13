1e889255a1dce9064fd05cd4110b029e208be2e7
Michal Švácha <michal.svacha@gmail.com>
2017-05-13 16:43:34 +0200

    IndexPathExtension tests

---
+++ GigsterousTests/Classes/Helpers/IndexPathExtensionTests.swift
@@ -0,0 +1,33 @@
+//
+//  IndexPathExtensionTests.swift
+//  Gigsterous
+//
+//  Created by Michal Svacha on 13.05.17.
+//  Copyright © 2017 Svacha, Michal. All rights reserved.
+//
+
+import XCTest
+
+class IndexPathExtensionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testIndexPathEquality() {
+        let indexPath = IndexPath(row: 2, section: 3)
+        let indexPathSame = IndexPath(row: 2, section: 3)
+        let indexPathDifferentRow = IndexPath(row: 1, section: 3)
+        let indexPathDifferentSection = IndexPath(row: 2, section: 4)
+        let indexPathCompletelyDifferent = IndexPath(row: 1, section: 4)
+        
+        XCTAssertTrue(indexPath.isEqual(other: indexPathSame))
+        XCTAssertFalse(indexPath.isEqual(other: indexPathDifferentRow))
+        XCTAssertFalse(indexPath.isEqual(other: indexPathDifferentSection))
+        XCTAssertFalse(indexPath.isEqual(other: indexPathCompletelyDifferent))
+    }
+}
